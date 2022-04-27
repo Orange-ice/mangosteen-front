296f686f180c1701c2c1721dd92b606200c31256
Burt <2902978956@qq.com>
2022-04-27 21:28:15 +0800

    使用 TSX 代替 template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    vueJsx({
+      // https://vitejs.dev/plugin-vue-jsx/
+      transformOn: true,
+      mergeProps: true,
+    }),
+  ],
+});
