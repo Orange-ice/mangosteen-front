d321a851ca0842ae4967541f5a7bd4334e4387f2
Burt <2902978956@qq.com>
2022-05-04 13:14:59 +0800

    欢迎界面完成

--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,26 +1,51 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
 import s from './welcome.module.scss';
 import logo from '../../assets/welcome/logo.svg';
 import pig from '../../assets/welcome/pig.svg';
+import clock from '../../assets/welcome/clock.svg';
+import chart from '../../assets/welcome/chart.svg';
+import cloud from '../../assets/welcome/cloud.svg';
+
+const contentList =[
+  {text: '会挣钱\n还要会省钱', img: pig},
+  {text: '每日提醒\n不会遗漏每一笔账单', img: clock},
+  {text: '数据可视化\n收支一目了然', img: chart},
+  {text: '云备份\n再也不怕数据丢失', img: cloud}
+]
 
 const Welcome = defineComponent({
     setup() {
+      const refStep = ref(0);
+      const router = useRouter();
+      const nextStep = () => {
+        if(refStep.value < 3 ) {
+          refStep.value += 1;
+          return;
+        }
+        router.push('/');
+      }
         return () => (
             <div class={s.wrapper}>
                 <header class={s.header}>
                     <img src={logo} alt="logo"/>
                     <h2>山竹记账</h2>
                 </header>
-                <main class={s.main}>
-                    <div class={s.content}>
-                        <img src={pig} alt="存钱罐配图"/>
-                        <p>会挣钱<br/>还要会省钱</p>
+                <main class={s.main} style={{transform: `translateX(-${refStep.value}00%)`}}>
+
+                  {contentList.map(item => (
+                    <div key={item.img} class={s.content}>
+                      <img src={item.img} alt=""/>
+                      {/*  p{ white-space: pre-line; }  可以使得 html中 \n 显示为换行符 */}
+                      <p style={{whiteSpace: 'pre-line'}}>{item.text}</p>
                     </div>
+                  ))}
+
                 </main>
                 <footer class={s.footer}>
                     {/* 假按钮，为了 css 布局占位 */}
                     <button class={s.fake}>跳过</button>
-                    <button>下一页</button>
+                    <button onClick={nextStep}>下一页</button>
                     <button>跳过</button>
                 </footer>
             </div>
