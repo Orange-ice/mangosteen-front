9e1da2e57ff1ede4e5c1c397f6891a900983d588
Burt <2902978956@qq.com>
2022-05-10 20:22:13 +0800

    完善 overlay 样式

--- src/components/overlay/index.tsx
+++ src/components/overlay/index.tsx
@@ -1,37 +1,45 @@
 import {defineComponent, PropType} from 'vue';
+import {RouterLink} from 'vue-router';
 import s from './overlay.module.scss';
 import Icon from '../icon';
 
 const Overlay = defineComponent({
-  props:{
+  props: {
     visible: {
       type: Boolean as PropType<boolean>,
+      required: true,
     },
     onClose: {
       type: Function as PropType<() => void>,
     }
   },
   setup(props) {
-    return () => <div style={{display: props.visible ? 'block': 'none'}}>
+    return () => <div style={{display: props.visible ? 'block' : 'none'}}>
       <div class={s.mask} onClick={props.onClose}/>
       <div class={s.overlay}>
-        <section>
+        <section class={s.user}>
           <h2>未登录用户</h2>
           <p>点击这里登录</p>
         </section>
         <nav>
-          <ul>
+          <ul class={s.action_list}>
             <li>
-              <Icon name="diagram"/>
-              <span>统计图表</span>
+              <RouterLink to="/statistics" class={s.action}>
+                <Icon name="diagram"/>
+                <span>统计图表</span>
+              </RouterLink>
             </li>
             <li>
-              <Icon name="export"/>
-              <span>导出数据</span>
+              <RouterLink to="/export" class={s.action}>
+                <Icon name="export"/>
+                <span>导出数据</span>
+              </RouterLink>
             </li>
             <li>
-              <Icon name="notify"/>
-              <span>记账提醒</span>
+              <RouterLink to="/notify" class={s.action}>
+                <Icon name="notify"/>
+                <span>记账提醒</span>
+              </RouterLink>
             </li>
           </ul>
         </nav>
