a497b26c51d9fdf2e337f2d16075c8292100a389
Burt <2902978956@qq.com>
2022-05-15 20:06:24 +0800

    WIP: /items/create 页面

--- src/views/item/item_create/index.tsx
+++ src/views/item/item_create/index.tsx
@@ -1,11 +1,24 @@
 import {defineComponent} from 'vue';
+import MainLayout from '../../../components/mainLayout';
+import Icon from '../../../components/icon';
+import {useRouter} from 'vue-router';
 
 const ItemCreate = defineComponent({
   setup() {
+    const router = useRouter();
+
+    const goBack = () => {
+      router.back();
+    };
+
     return () => (
-      <div>
-        <h1>ItemCreate</h1>
-      </div>
+      <MainLayout>{{
+        title: () => '记一笔',
+        icon: () => <Icon name="back" onClick={goBack}/>,
+        default: () => <>
+          <div>记一笔</div>
+        </>,
+      }}</MainLayout>
     );
   }
 });
