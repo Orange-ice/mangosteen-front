1e5255a1bb1a0fc8cc8a6cbedc65bbbb215f94d8
burt <2902978956@qq.com>
2022-05-18 14:30:21 +0800

    WIP: 日期及金额样式

--- src/views/item/item_create/input_pad.tsx
+++ src/views/item/item_create/input_pad.tsx
@@ -5,13 +5,17 @@ const InputPad = defineComponent({
   setup() {
     const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '清空', '提交'];
     const append = (value: string) => {}
-    return () => (
+    return () => <>
+      <div class={s.date_amount}>
+        <input type="date" value={'2022-05-20'} />
+        <span class={s.amount}>199.99</span>
+      </div>
       <div class={s.button_wrapper}>
         {buttons.map(item => (
           <button onClick={() => append(item)}>{item}</button>
         ))}
       </div>
-    );
+    </>
   }
 });
 
