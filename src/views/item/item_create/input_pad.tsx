c99adcc71d17582ce69736acb69d0074486b5fa2
burt <2902978956@qq.com>
2022-05-18 11:13:40 +0800

    WIP: 数字键盘样式

--- src/views/item/item_create/input_pad.tsx
+++ src/views/item/item_create/input_pad.tsx
@@ -1,13 +1,19 @@
 import {defineComponent} from 'vue';
-import s from './input_pad.scss';
+import s from './input_pad.module.scss';
 
 const InputPad = defineComponent({
-  setup(){
+  setup() {
+    const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '清空', '提交'];
+    const append = (value: string) => {}
     return () => (
-      <div>InputPad</div>
-    )
+      <div class={s.button_wrapper}>
+        {buttons.map(item => (
+          <button onClick={() => append(item)}>{item}</button>
+        ))}
+      </div>
+    );
   }
-})
+});
 
 
 export default InputPad;
